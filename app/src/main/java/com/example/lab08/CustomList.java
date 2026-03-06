55016e459f8dd29ad23ed8a063deed03c3de0803
Jigarjeet Mannan <jigarjeetmannan@gmail.com>
2026-03-06 13:51:55 -0700

    corrected testCountCity

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -26,9 +26,9 @@ public class CustomList {
      }
 
      // Implement after failing
-//     public int cityCount() {
-//        return cities.size();
-//     }
+     public int cityCount() {
+        return cities.size();
+     }
 
 
 }
